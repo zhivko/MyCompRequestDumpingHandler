4b5b18fada93d464f4725c100b69a69fbf3ecc43
klemen zivkovic <klemen.zivkovic@gmail.com>
2018-09-06 10:18:33 +0200

    modified:   pom.xml 	modified:   src/main/java/si/mycomp/requestDumpingHandler/DfsRequestDumpingHandler.java 	.gitignore

--- src/main/java/si/mycomp/requestDumpingHandler/DfsRequestDumpingHandler.java
+++ src/main/java/si/mycomp/requestDumpingHandler/DfsRequestDumpingHandler.java
@@ -6,6 +6,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.xnio.ChannelListener;
@@ -16,9 +19,11 @@ import io.undertow.UndertowLogger;
 import io.undertow.connector.PooledByteBuffer;
 import io.undertow.server.Connectors;
 import io.undertow.server.ExchangeCompletionListener;
+import io.undertow.server.HandlerWrapper;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
-import io.undertow.server.protocol.http.HttpContinue;
+import io.undertow.server.handlers.RequestBufferingHandler;
+import io.undertow.server.handlers.builder.HandlerBuilder;
 
 public class DfsRequestDumpingHandler implements HttpHandler {
 
@@ -33,146 +38,144 @@ public class DfsRequestDumpingHandler implements HttpHandler {
 	String requestStr;
 	int maxBuffers = 1024;
 
-	public DfsRequestDumpingHandler(HttpHandler next) {
+	public DfsRequestDumpingHandler(HttpHandler next, int maxBuffers2) {
 		this.next = next;
+		this.maxBuffers = maxBuffers2;
 	}
 
 	@Override
 	public void handleRequest(final HttpServerExchange exchange) throws Exception {
-		StringBuilder builder=new StringBuilder();
+		StringBuilder builder = new StringBuilder();
 
-		//if (!exchange.isRequestComplete() && !HttpContinue.requiresContinueResponse(exchange.getRequestHeaders())) {
-			final StreamSourceChannel channel = exchange.getRequestChannel();
-			int readBuffers = 0;
-			final PooledByteBuffer[] bufferedData = new PooledByteBuffer[maxBuffers];
-			PooledByteBuffer buffer = exchange.getConnection().getByteBufferPool().allocate();
-			System.out.println("1");
-			try {
-				do {
-					int r;
-					ByteBuffer b = buffer.getBuffer();
-					r = channel.read(b);
-					if (r == -1) {
-						if (b.position() == 0) {
-							buffer.close();
-						} else {
-							b.flip();
-							System.out.println("2");
-							bufferedData[readBuffers] = buffer;
-						}
-						break;
-					} else if (r == 0) {
-						final PooledByteBuffer finalBuffer = buffer;
-						final int finalReadBuffers = readBuffers;
-						channel.getReadSetter().set(new ChannelListener<StreamSourceChannel>() {
+		// if (!exchange.isRequestComplete() &&
+		// !HttpContinue.requiresContinueResponse(exchange.getRequestHeaders())) {
+		final StreamSourceChannel channel = exchange.getRequestChannel();
+		int readBuffers = 0;
+		final PooledByteBuffer[] bufferedData = new PooledByteBuffer[maxBuffers];
+		PooledByteBuffer buffer = exchange.getConnection().getByteBufferPool().allocate();
+		System.out.println("1");
+		try {
+			do {
+				int r;
+				ByteBuffer b = buffer.getBuffer();
+				r = channel.read(b);
+				if (r == -1) {
+					if (b.position() == 0) {
+						buffer.close();
+					} else {
+						b.flip();
+						System.out.println("2");
+						bufferedData[readBuffers] = buffer;
+					}
+					break;
+				} else if (r == 0) {
+					final PooledByteBuffer finalBuffer = buffer;
+					final int finalReadBuffers = readBuffers;
+					channel.getReadSetter().set(new ChannelListener<StreamSourceChannel>() {
 
-							PooledByteBuffer buffer = finalBuffer;
-							int readBuffers = finalReadBuffers;
+						PooledByteBuffer buffer = finalBuffer;
+						int readBuffers = finalReadBuffers;
 
-							@Override
-							public void handleEvent(StreamSourceChannel channel) {
-								try {
-									do {
-										int r;
-										ByteBuffer b = buffer.getBuffer();
-										r = channel.read(b);
-										if (r == -1) {
-											if (b.position() == 0) {
-												buffer.close();
-											} else {
-												b.flip();
-												System.out.println("3");
-												bufferedData[readBuffers] = buffer;
-											}
+						@Override
+						public void handleEvent(StreamSourceChannel channel) {
+							try {
+								do {
+									int r;
+									ByteBuffer b = buffer.getBuffer();
+									r = channel.read(b);
+									if (r == -1) {
+										if (b.position() == 0) {
+											buffer.close();
+										} else {
+											b.flip();
+											System.out.println("3");
+											bufferedData[readBuffers] = buffer;
+										}
+										Connectors.ungetRequestBytes(exchange, bufferedData);
+										Connectors.resetRequestChannel(exchange);
+										channel.getReadSetter().set(null);
+										channel.suspendReads();
+										Connectors.executeRootHandler(next, exchange);
+										return;
+									} else if (r == 0) {
+										return;
+									} else if (!b.hasRemaining()) {
+										b.flip();
+										System.out.println("4");
+										bufferedData[readBuffers++] = buffer;
+										if (readBuffers == maxBuffers) {
 											Connectors.ungetRequestBytes(exchange, bufferedData);
 											Connectors.resetRequestChannel(exchange);
 											channel.getReadSetter().set(null);
 											channel.suspendReads();
 											Connectors.executeRootHandler(next, exchange);
 											return;
-										} else if (r == 0) {
-											return;
-										} else if (!b.hasRemaining()) {
-											b.flip();
-											System.out.println("4");
-											bufferedData[readBuffers++] = buffer;
-											if (readBuffers == maxBuffers) {
-												Connectors.ungetRequestBytes(exchange, bufferedData);
-												Connectors.resetRequestChannel(exchange);
-												channel.getReadSetter().set(null);
-												channel.suspendReads();
-												Connectors.executeRootHandler(next, exchange);
-												return;
-											}
-											buffer = exchange.getConnection().getByteBufferPool().allocate();
 										}
-									} while (true);
-								} catch (Throwable t) {
-									if (t instanceof IOException) {
-										UndertowLogger.REQUEST_IO_LOGGER.ioException((IOException) t);
-									} else {
-										UndertowLogger.REQUEST_IO_LOGGER.handleUnexpectedFailure(t);
+										buffer = exchange.getConnection().getByteBufferPool().allocate();
 									}
-									for (int i = 0; i < bufferedData.length; ++i) {
-										IoUtils.safeClose(bufferedData[i]);
-									}
-									if (buffer != null && buffer.isOpen()) {
-										IoUtils.safeClose(buffer);
-									}
-									exchange.endExchange();
+								} while (true);
+							} catch (Throwable t) {
+								if (t instanceof IOException) {
+									UndertowLogger.REQUEST_IO_LOGGER.ioException((IOException) t);
+								} else {
+									UndertowLogger.REQUEST_IO_LOGGER.handleUnexpectedFailure(t);
 								}
+								for (int i = 0; i < bufferedData.length; ++i) {
+									IoUtils.safeClose(bufferedData[i]);
+								}
+								if (buffer != null && buffer.isOpen()) {
+									IoUtils.safeClose(buffer);
+								}
+								exchange.endExchange();
 							}
-						});
-						channel.resumeReads();
-						return;
-					} else if (!b.hasRemaining()) {
-						b.flip();
-						bufferedData[readBuffers++] = buffer;
-						if (readBuffers == maxBuffers) {
-							break;
 						}
-						buffer = exchange.getConnection().getByteBufferPool().allocate();
+					});
+					channel.resumeReads();
+					return;
+				} else if (!b.hasRemaining()) {
+					b.flip();
+					bufferedData[readBuffers++] = buffer;
+					if (readBuffers == maxBuffers) {
+						break;
 					}
-					
-				} while (true);
-				
-				System.out.println("length: " + readBuffers);
-				for(int i=0;i<=readBuffers;i++)
-				{
-					byte[] bytes;
-					System.out.println("i:" + i);
-					System.out.println(bufferedData[i].getBuffer());
-			    if(bufferedData[i].getBuffer().hasArray()) {
-			        bytes = bufferedData[i].getBuffer().array();
-			    } else {
-			        bytes = new byte[bufferedData[i].getBuffer().remaining()];
-			        bufferedData[i].getBuffer().get(bytes);
-			    }
-					String str = new String(bytes, StandardCharsets.UTF_8);
-					builder.append(str);
-					builder.append(System.lineSeparator());					
-				}							
-				
-				Connectors.ungetRequestBytes(exchange, bufferedData);
-				Connectors.resetRequestChannel(exchange);
+					buffer = exchange.getConnection().getByteBufferPool().allocate();
+				}
 
-			} catch (Exception | Error e) {
-				for (int i = 0; i < bufferedData.length; ++i) {
-					IoUtils.safeClose(bufferedData[i]);
+			} while (true);
+
+			System.out.println("length: " + readBuffers);
+			for (int i = 0; i <= readBuffers; i++) {
+				byte[] bytes;
+				System.out.println("i:" + i);
+				System.out.println(bufferedData[i].getBuffer());
+				if (bufferedData[i].getBuffer().hasArray()) {
+					bytes = bufferedData[i].getBuffer().array();
+				} else {
+					bytes = new byte[bufferedData[i].getBuffer().remaining()];
+					bufferedData[i].getBuffer().get(bytes);
 				}
-				if (buffer != null && buffer.isOpen()) {
-					IoUtils.safeClose(buffer);
-				}
-				throw e;
+				String str = new String(bytes, StandardCharsets.UTF_8);
+				builder.append(str);
+				builder.append(System.lineSeparator());
 			}
 
-	
-			
-			PostExchangeListener mylistener = new PostExchangeListener(builder.toString(), System.nanoTime());
-			exchange.addExchangeCompleteListener(mylistener);
-		//	}
+			Connectors.ungetRequestBytes(exchange, bufferedData);
+			Connectors.resetRequestChannel(exchange);
 
-	
+		} catch (Exception | Error e) {
+			for (int i = 0; i < bufferedData.length; ++i) {
+				IoUtils.safeClose(bufferedData[i]);
+			}
+			if (buffer != null && buffer.isOpen()) {
+				IoUtils.safeClose(buffer);
+			}
+			throw e;
+		}
+
+		PostExchangeListener mylistener = new PostExchangeListener(builder.toString(), System.nanoTime());
+		exchange.addExchangeCompleteListener(mylistener);
+		// }
+
 		next.handleRequest(exchange);
 	}
 
@@ -219,4 +222,47 @@ public class DfsRequestDumpingHandler implements HttpHandler {
 			return br.lines().collect(Collectors.joining(System.lineSeparator()));
 		}
 	}
+
+	public static final class Wrapper implements HandlerWrapper {
+
+		private final int maxBuffers;
+
+		public Wrapper(int maxBuffers) {
+			this.maxBuffers = maxBuffers;
+		}
+
+		@Override
+		public HttpHandler wrap(HttpHandler handler) {
+			return new DfsRequestDumpingHandler(handler, maxBuffers);
+		}
+	}
+
+	public static final class Builder implements HandlerBuilder {
+
+		@Override
+		public String name() {
+			return "buffer-request";
+		}
+
+		@Override
+		public Map<String, Class<?>> parameters() {
+			return Collections.<String, Class<?>> singletonMap("buffers", Integer.class);
+		}
+
+		@Override
+		public Set<String> requiredParameters() {
+			return Collections.singleton("buffers");
+		}
+
+		@Override
+		public String defaultParameter() {
+			return "buffers";
+		}
+
+		@Override
+		public HandlerWrapper build(Map<String, Object> config) {
+			return new Wrapper((Integer) config.get("buffers"));
+		}
+	}
+
 }
